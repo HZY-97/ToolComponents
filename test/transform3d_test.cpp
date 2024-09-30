e8f476185152ca2b624fab34861905ba069aab3c
huizeyu <huizeyu@siasun.com>
2024-09-30 10:17:40 +0800

    四元数的相关转换及测试完成

--- test/transform3d_test.cpp
+++ test/transform3d_test.cpp
@@ -14,6 +14,8 @@
 
 #include "Eigen/src/Geometry/Quaternion.h"
 #include "share_data_test.hpp"
+
+// tf::ToDeg
 TEST(TestTransform3d, HandlesToDeg) {
   double toDeg = tf::ToDeg(rad_45);
   cout << "toDeg=" << toDeg << seg;
@@ -21,6 +23,7 @@ TEST(TestTransform3d, HandlesToDeg) {
   EXPECT_LE(ret, 1e-4) << "rad_45-deg_45=" << ret;
 }
 
+// tf::ToRad
 TEST(TestTransform3d, HandlesToRad) {
   double toRad = tf::ToRad(deg_45);
   cout << "toRad=" << toRad << seg;
@@ -28,6 +31,33 @@ TEST(TestTransform3d, HandlesToRad) {
   EXPECT_LE(ret, 1e-4) << "deg_45-rad_45=" << ret;
 }
 
+// tf::EulerRad2Mat
+TEST(TestTransform3d, HandlesEulerV3dRad2Mat) {
+  auto V3d = Vector3d(rad_30, rad_20, rad_10);
+  auto Mat = tf::EulerRad2Mat(V3d);
+  Matrix3d ret = Mat - r.roll10_pitch20_yaw30_mat;
+  cout << "v3d ret:\n" << ret << seg;
+  for (int i = 0; i < ret.rows(); i++) {
+    for (int j = 0; j < ret.cols(); j++) {
+      ASSERT_LE(std::abs(ret(i, j)), 1e-4);
+    }
+  }
+}
+
+// tf::EulerDeg2Mat
+TEST(TestTransform3d, HandlesEulerV3dDeg2Mat) {
+  auto V3d = Vector3d(deg_30, deg_20, deg_10);
+  auto Mat = tf::EulerDeg2Mat(V3d);
+  Matrix3d ret = Mat - r.roll10_pitch20_yaw30_mat;
+  cout << "v3d ret:\n" << ret << seg;
+  for (int i = 0; i < ret.rows(); i++) {
+    for (int j = 0; j < ret.cols(); j++) {
+      ASSERT_LE(std::abs(ret(i, j)), 1e-4);
+    }
+  }
+}
+
+// tf::EulerDeg2Mat
 TEST(TestTransform3d, HandlesEulerDeg2Mat) {
   Rot3d r;
   Matrix3d mat = tf::EulerDeg2Mat(45, 45, 45);
@@ -41,6 +71,7 @@ TEST(TestTransform3d, HandlesEulerDeg2Mat) {
   }
 }
 
+// tf::EulerRad2Mat
 TEST(TestTransform3d, HandlesEulerRad2Mat) {
   Matrix3d rad_mat =
       tf::EulerRad2Mat(tf::ToRad(45), tf::ToRad(45), tf::ToRad(45));
@@ -54,6 +85,7 @@ TEST(TestTransform3d, HandlesEulerRad2Mat) {
   }
 }
 
+// tf::EulerDeg2Quat
 TEST(TestTransform3d, HandlesEulerDeg2Quat) {
   Quaterniond q = tf::EulerDeg2Quat(45, 45, 45);
   cout << "45,45,45 quat= \n" << q << seg;
@@ -71,6 +103,7 @@ TEST(TestTransform3d, HandlesEulerDeg2Quat) {
   ASSERT_LE(det_q_w, 1e-4);
 }
 
+// tf::EulerRad2Quat
 TEST(TestTransform3d, HandlesEulerRad2Quat) {
   Quaterniond q =
       tf::EulerRad2Quat(tf::ToRad(45), tf::ToRad(45), tf::ToRad(45));
@@ -89,6 +122,25 @@ TEST(TestTransform3d, HandlesEulerRad2Quat) {
   ASSERT_LE(det_q_w, 1e-4);
 }
 
+// tf::Mat2EulerRad
+TEST(TestTransform3d, HandlesMat2EulerRadV3d) {
+  Vector3d euler_rad = tf::Mat2EulerRad(r.roll10_pitch20_yaw30_mat);
+  cout << "euler_rad=\n" << euler_rad << seg;
+  ASSERT_LE(abs(euler_rad[0] - rad_30), 1e-4);
+  ASSERT_LE(abs(euler_rad[1] - rad_20), 1e-4);
+  ASSERT_LE(abs(euler_rad[2] - rad_10), 1e-4);
+}
+
+// tf::Mat2EulerDeg
+TEST(TestTransform3d, HandlesMat2EulerDegV3d) {
+  Vector3d euler_degs = tf::Mat2EulerDeg(r.roll10_pitch20_yaw30_mat);
+  cout << "euler_degs=\n" << euler_degs << seg;
+  ASSERT_LE(abs(euler_degs[0] - deg_30), 1e-4);
+  ASSERT_LE(abs(euler_degs[1] - deg_20), 1e-4);
+  ASSERT_LE(abs(euler_degs[2] - deg_10), 1e-4);
+}
+
+// tf::Mat2EulerDeg
 TEST(TestTransform3d, HandlesMat2EulerDeg) {
   double roll_deg;
   double pitch_deg;
@@ -100,6 +152,7 @@ TEST(TestTransform3d, HandlesMat2EulerDeg) {
   ASSERT_LE(abs(yaw_deg - deg_30), 1e-4);
 }
 
+// tf::Mat2EulerRad
 TEST(TestTransform3d, HandlesMat2EulerRad) {
   double roll_rad;
   double pitch_rad;
@@ -111,6 +164,7 @@ TEST(TestTransform3d, HandlesMat2EulerRad) {
   ASSERT_LE(abs(yaw_rad - rad_30), 1e-4);
 }
 
+// tf::Mat2Quat
 TEST(TestTransform3d, HandlesMat2Quat) {
   Quaterniond q = tf::Mat2Quat(r.roll10_pitch20_yaw30_mat);
 
@@ -128,4 +182,58 @@ TEST(TestTransform3d, HandlesMat2Quat) {
   ASSERT_LE(det_q_y, 1e-4);
   ASSERT_LE(det_q_z, 1e-4);
   ASSERT_LE(det_q_w, 1e-4);
+}
+
+// tf::Quat2EulerRad
+TEST(TestTransform3d, HandlesQuat2EulerRadV3d) {
+  Vector3d rad = tf::Quat2EulerRad(r.roll10_pitch20_yaw30_quat);
+  cout << "rad=\n" << rad << seg;
+  ASSERT_LE(abs(rad(0) - rad_30), 1e-4);
+  ASSERT_LE(abs(rad(1) - rad_20), 1e-4);
+  ASSERT_LE(abs(rad(2) - rad_10), 1e-4);
+}
+
+// tf::Quat2EulerDeg
+TEST(TestTransform3d, HandlesQuat2EulerDegV3d) {
+  Vector3d deg = tf::Quat2EulerDeg(r.roll10_pitch20_yaw30_quat);
+  cout << "deg=\n" << deg << seg;
+  ASSERT_LE(abs(deg(0) - deg_30), 1e-4);
+  ASSERT_LE(abs(deg(1) - deg_20), 1e-4);
+  ASSERT_LE(abs(deg(2) - deg_10), 1e-4);
+}
+
+// tf::Quat2EulerRad
+TEST(TestTransform3d, HandlesQuat2EulerRad) {
+  double roll_rad, pitch_rad, yaw_rad;
+  tf::Quat2EulerRad(r.roll10_pitch20_yaw30_quat, roll_rad, pitch_rad, yaw_rad);
+  cout << "roll_rad=" << roll_rad << seg;
+  cout << "pitch_rad=" << pitch_rad << seg;
+  cout << "yaw_rad=" << yaw_rad << seg;
+  ASSERT_LE(abs(roll_rad - rad_10), 1e-4);
+  ASSERT_LE(abs(pitch_rad - rad_20), 1e-4);
+  ASSERT_LE(abs(yaw_rad - rad_30), 1e-4);
+}
+
+// tf::Quat2EulerDeg
+TEST(TestTransform3d, HandlesQuat2EulerDeg) {
+  double roll_deg, pitch_deg, yaw_deg;
+  tf::Quat2EulerDeg(r.roll10_pitch20_yaw30_quat, roll_deg, pitch_deg, yaw_deg);
+  cout << "roll_deg=" << roll_deg << seg;
+  cout << "pitch_deg=" << pitch_deg << seg;
+  cout << "yaw_deg=" << yaw_deg << seg;
+  ASSERT_LE(abs(roll_deg - deg_10), 1e-4);
+  ASSERT_LE(abs(pitch_deg - deg_20), 1e-4);
+  ASSERT_LE(abs(yaw_deg - deg_30), 1e-4);
+}
+
+// tf::Quat2Mat
+TEST(TestTransform3d, HandlesQuat2Mat) {
+  Matrix3d rot_mat = tf::Quat2Mat(r.roll10_pitch20_yaw30_quat);
+  Matrix3d ret = rot_mat - r.roll10_pitch20_yaw30_mat;
+  cout << "10,20,30 rot_mat-roll10_pitch20_yaw30_mat= \n" << ret << seg;
+  for (int i = 0; i < ret.rows(); i++) {
+    for (int j = 0; j < ret.cols(); j++) {
+      ASSERT_LE(std::abs(ret(i, j)), 1e-4);
+    }
+  }
 }
--- transform3d/transform3d.h
+++ transform3d/transform3d.h
@@ -18,8 +18,8 @@
 // 旋转矩阵转欧拉角 角度 弧度===============O
 // 旋转矩阵转四元数 ======================O
 
-// 四元数转欧拉角  角度 弧度
-// 四元数转旋转矩阵
+// 四元数转欧拉角  角度 弧度===============O
+// 四元数转旋转矩阵=======================O
 
 // 齐次矩阵相关
 
@@ -70,6 +70,37 @@ class Transform3D {
   /*===============欧拉角转换================*/
 
   /**
+   * @brief 弧度表示的欧拉角转换为旋转矩阵，按ZYX的顺序依次旋转
+   *
+   * @param euler_rad 弧度表示 yaw:v[0] pitch:v[1] roll[2]
+   * @return Matrix3d 旋转矩阵
+   */
+  static inline Matrix3d EulerRad2Mat(Vector3d euler_rad) {
+    AngleAxisd rollAngle(AngleAxisd(euler_rad[2], Vector3d::UnitX()));
+    AngleAxisd pitchAngle(AngleAxisd(euler_rad[1], Vector3d::UnitY()));
+    AngleAxisd yawAngle(AngleAxisd(euler_rad[0], Vector3d::UnitZ()));
+    Matrix3d rotation_matrix;
+    rotation_matrix = yawAngle * pitchAngle * rollAngle;
+    return rotation_matrix;
+  }
+
+  /**
+   * @brief 角度表示的欧拉角转换为旋转矩阵，按ZYX的顺序依次旋转
+   *
+   * @param euler_deg 角度表示 yaw:v[0] pitch:v[1] roll[2]
+   * @return Matrix3d 旋转矩阵
+   */
+  static inline Matrix3d EulerDeg2Mat(Vector3d euler_deg) {
+    euler_deg = euler_deg * M_PI / 180.0;
+    AngleAxisd rollAngle(AngleAxisd(euler_deg[2], Vector3d::UnitX()));
+    AngleAxisd pitchAngle(AngleAxisd(euler_deg[1], Vector3d::UnitY()));
+    AngleAxisd yawAngle(AngleAxisd(euler_deg[0], Vector3d::UnitZ()));
+    Matrix3d rotation_matrix;
+    rotation_matrix = yawAngle * pitchAngle * rollAngle;
+    return rotation_matrix;
+  }
+
+  /**
    * @brief 角度表示的欧拉角转换为旋转矩阵，按ZYX的顺序依次旋转
    *
    * @param roll_deg 角度表示的roll   绕x旋转
@@ -79,15 +110,8 @@ class Transform3D {
    */
   static inline Matrix3d EulerDeg2Mat(double roll_deg, double pitch_deg,
                                       double yaw_deg) {
-    roll_deg = ToRad(roll_deg);
-    pitch_deg = ToRad(pitch_deg);
-    yaw_deg = ToRad(yaw_deg);
-    AngleAxisd rollAngle(AngleAxisd(roll_deg, Vector3d::UnitX()));
-    AngleAxisd pitchAngle(AngleAxisd(pitch_deg, Vector3d::UnitY()));
-    AngleAxisd yawAngle(AngleAxisd(yaw_deg, Vector3d::UnitZ()));
-    Matrix3d rotation_matrix;
-    rotation_matrix = yawAngle * pitchAngle * rollAngle;
-    return rotation_matrix;
+    Vector3d euler_deg{yaw_deg, pitch_deg, roll_deg};
+    return EulerDeg2Mat(euler_deg);
   }
 
   /**
@@ -100,12 +124,8 @@ class Transform3D {
    */
   static inline Matrix3d EulerRad2Mat(double roll_rad, double pitch_rad,
                                       double yaw_rad) {
-    AngleAxisd rollAngle(AngleAxisd(roll_rad, Vector3d::UnitX()));
-    AngleAxisd pitchAngle(AngleAxisd(pitch_rad, Vector3d::UnitY()));
-    AngleAxisd yawAngle(AngleAxisd(yaw_rad, Vector3d::UnitZ()));
-    Matrix3d rotation_matrix;
-    rotation_matrix = yawAngle * pitchAngle * rollAngle;
-    return rotation_matrix;
+    Vector3d euler_rad{yaw_rad, pitch_rad, roll_rad};
+    return EulerRad2Mat(euler_rad);
   }
 
   /**
@@ -118,12 +138,9 @@ class Transform3D {
    */
   static inline Quaterniond EulerDeg2Quat(double roll_deg, double pitch_deg,
                                           double yaw_deg) {
-    roll_deg = ToRad(roll_deg);
-    pitch_deg = ToRad(pitch_deg);
-    yaw_deg = ToRad(yaw_deg);
-    return Eigen::AngleAxisd(yaw_deg, ::Eigen::Vector3d::UnitZ()) *
-           Eigen::AngleAxisd(pitch_deg, ::Eigen::Vector3d::UnitY()) *
-           Eigen::AngleAxisd(roll_deg, ::Eigen::Vector3d::UnitX());
+    return Eigen::AngleAxisd(ToRad(yaw_deg), ::Eigen::Vector3d::UnitZ()) *
+           Eigen::AngleAxisd(ToRad(pitch_deg), ::Eigen::Vector3d::UnitY()) *
+           Eigen::AngleAxisd(ToRad(roll_deg), ::Eigen::Vector3d::UnitX());
   }
 
   /**
@@ -144,6 +161,26 @@ class Transform3D {
   /*===============欧拉角转换================*/
 
   /*===============旋转矩阵转换================*/
+
+  /**
+   * @brief 旋转矩阵转弧度制欧拉角 按ZYX的顺序依次旋转
+   *
+   * @param rot_mat 旋转矩阵
+   * @return Vector3d yaw:v[0] pitch:v[1] roll[2]
+   */
+  static inline Vector3d Mat2EulerRad(Matrix3d rot_mat) {
+    return rot_mat.eulerAngles(2, 1, 0);
+  }
+
+  /**
+   * @brief 旋转矩阵转角度制欧拉角 按ZYX的顺序依次旋转
+   *
+   * @param rot_mat 旋转矩阵
+   * @return Vector3d yaw:v[0] pitch:v[1] roll[2]
+   */
+  static inline Vector3d Mat2EulerDeg(Matrix3d rot_mat) {
+    return rot_mat.eulerAngles(2, 1, 0) * 180 / M_PI;
+  }
 
   /**
    * @brief 旋转矩阵转角度制欧拉角 按ZYX的顺序依次旋转
@@ -183,6 +220,72 @@ class Transform3D {
   }
 
   /*===============旋转矩阵转换================*/
+
+  /*===============四元数转换================*/
+
+  /**
+   * @brief 四元数转弧度制欧拉角 按ZYX的顺序依次旋转
+   *
+   * @param quat 待转换的四元数
+   * @return Vector3d 弧度表示 yaw:v[0] pitch:v[1] roll[2]
+   */
+  static inline Vector3d Quat2EulerRad(Quaterniond quat) {
+    return quat.toRotationMatrix().eulerAngles(2, 1, 0);
+  }
+
+  /**
+   * @brief 四元数转角度制欧拉角 按ZYX的顺序依次旋转
+   *
+   * @param quat
+   * @return Vector3d 角度表示 yaw:v[0] pitch:v[1] roll[2]
+   */
+  static inline Vector3d Quat2EulerDeg(Quaterniond quat) {
+    return Quat2EulerRad(quat) * 180 / M_PI;
+  }
+
+  /**
+   * @brief 四元数转弧度制欧拉角 按ZYX的顺序依次旋转
+   *
+   * @param quat       待转换的四元数
+   * @param roll_rad   出参 弧度表示的roll
+   * @param pitch_rad  出参 弧度表示的pitch
+   * @param yaw_rad    出参 弧度表示的yaw
+   */
+  static inline void Quat2EulerRad(Quaterniond quat, double &roll_rad,
+                                   double &pitch_rad, double &yaw_rad) {
+    Vector3d Euler = Quat2EulerRad(quat);
+    roll_rad = Euler(2);
+    pitch_rad = Euler(1);
+    yaw_rad = Euler(0);
+  }
+
+  /**
+   * @brief 四元数转角度制欧拉角 按ZYX的顺序依次旋转
+   *
+   * @param quat      待转换的四元数
+   * @param roll_deg  出参 角度表示的roll
+   * @param pitch_deg 出参 角度表示的pitch
+   * @param yaw_deg   出参 角度表示的yaw
+   */
+  static inline void Quat2EulerDeg(Quaterniond quat, double &roll_deg,
+                                   double &pitch_deg, double &yaw_deg) {
+    Vector3d Euler = Quat2EulerDeg(quat);
+    roll_deg = Euler(2);
+    pitch_deg = Euler(1);
+    yaw_deg = Euler(0);
+  }
+
+  /**
+   * @brief 四元数转旋转矩阵
+   *
+   * @param quat 待转换的四元数
+   * @return Matrix3d 旋转矩阵
+   */
+  static inline Matrix3d Quat2Mat(Quaterniond quat) {
+    return quat.toRotationMatrix();
+  }
+
+  /*===============四元数转换================*/
 
  private:
  public:
